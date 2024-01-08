f87f03fb78387792693a3ba4870fddefc81172f0
Uziel Menezes <uzielcivil@gmail.com>
2024-01-08 11:43:42 -0300

    feat: fetching categories for select options

--- src/components/form/select/Select.js
+++ src/components/form/select/Select.js
@@ -1,4 +1,4 @@
-import styles from "./Select.module.css";
+import styles from './Select.module.css';
 
 function Select({ text, name, options, handleOnChange, value }) {
   return (
@@ -6,6 +6,11 @@ function Select({ text, name, options, handleOnChange, value }) {
       <label htmlFor={name}> {text}</label>
       <select name={name} id={name}>
         <option>Select an option</option>
+        {options.map((option) => (
+          <option value={option.id} key={option.id}>
+            {option.name}
+          </option>
+        ))}
       </select>
     </div>
   );
--- src/components/project/project-form/ProjectForm.js
+++ src/components/project/project-form/ProjectForm.js
@@ -1,9 +1,26 @@
-import Input from "../../form/input/Input";
-import Select from "../../form/select/Select";
-import Submit from "../../form/submit/Submit";
-import styles from "./ProjectForm.module.css";
+import { useEffect, useState } from 'react';
+
+import Input from '../../form/input/Input';
+import Select from '../../form/select/Select';
+import Submit from '../../form/submit/Submit';
+import styles from './ProjectForm.module.css';
 
 function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data);
+      });
+  }, []);
+
   return (
     <form className={styles.form}>
       <Input
@@ -18,7 +35,11 @@ function ProjectForm({ btnText }) {
         name="budget"
         placeholder="Insert total budget"
       />
-      <Select name="category_id" text="Select a category" />
+      <Select
+        name="category_id"
+        text="Select a category"
+        options={categories}
+      />
       <Submit text={btnText} />
     </form>
   );
