0ccc634ed1c9ef88b1485ee96501c4bcf647ed1e
Uziel Menezes <uzielcivil@gmail.com>
2024-01-08 08:47:54 -0300

    feat: new project page and form idea

--- src/components/pages/new-project/NewProject.js
+++ src/components/pages/new-project/NewProject.js
@@ -1,5 +1,14 @@
+import ProjectForm from "../../project/project-form/ProjectForm";
+import styles from "./NewProject.module.css";
+
 function NewProject() {
-    return <h1>NewProject</h1>
-    }
-    
-    export default NewProject
+  return (
+    <div className={styles.new_project_container}>
+      <h1>Create Project</h1>
+      <p>Create your project then add services</p>
+      <ProjectForm />
+    </div>
+  );
+}
+
+export default NewProject;
