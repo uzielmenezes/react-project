c1d8dc6c6a91249d110e902f25827b65e0068e4c
Uziel Menezes <uzielcivil@gmail.com>
2024-01-08 08:26:57 -0300

    feat: home page finalized

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Container from "./components/layout/container/Container";
 import Footer from "./components/layout/footer/Footer";
 import NavBar from "./components/layout/nav-bar/NavBar";
-import Company from "./components/pages/Company";
-import Contact from "./components/pages/Contact";
-import Home from "./components/pages/Home";
-import NewProject from "./components/pages/NewProject";
-import Projects from "./components/pages/Projects";
+import Company from "./components/pages/company/Company";
+import Contact from "./components/pages/contact/Contact";
+import Home from "./components/pages/home/Home";
+import NewProject from "./components/pages/new-project/NewProject";
+import Projects from "./components/pages/projects/Projects";
 
 function App() {
   return (
