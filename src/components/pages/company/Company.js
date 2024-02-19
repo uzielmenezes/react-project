d74c9adf517fe2c425147f9a57895f58e750cd6a
Uziel <uzielcivil@gmail.com>
2024-02-19 20:04:22 -0300

    chore: content for company and contact pages

--- src/components/pages/company/Company.js
+++ src/components/pages/company/Company.js
@@ -1,5 +1,17 @@
+import Container from "../../layout/container/Container";
+
 function Company() {
-    return <h1>Company</h1>
-    }
-    
-    export default Company
+  return (
+    <Container customClass="column">
+      <h1>Company</h1>
+      <p>
+        Costs is a cutting-edge project manager operating at the intersection of
+        creativity and practicality.Founded by visionary entrepreneur Dr.
+        Cassandra Evergreen, This App has quickly become a powerhouse in the
+        fictional business landscape.
+      </p>
+    </Container>
+  );
+}
+
+export default Company;
--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -1,5 +1,18 @@
+import Container from "../../layout/container/Container";
+
 function Contact() {
-    return <h1>Contact</h1>
-    }
-    
-    export default Contact
+  return (
+    <Container customClass="column">
+      <h1>Contact</h1>
+      <h3>
+        E-mail:{" "}
+        <span style={{ fontWeight: "100" }}>cassandra_evergreen@costs.app</span>
+      </h3>
+      <h3>
+        Phone: <span style={{ fontWeight: "100" }}>+17696489153</span>
+      </h3>
+    </Container>
+  );
+}
+
+export default Contact;
